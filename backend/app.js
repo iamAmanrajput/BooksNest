b1e7aebaebe866c0670d04f13b943039975c5fe7
Aman Kumar <aman.it360@gmail.com>
2025-06-10 10:32:48 +0530

    Backend Setup

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>📚 Welcome to Library Backend</h1>");
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on port: ${PORT}`);
+});

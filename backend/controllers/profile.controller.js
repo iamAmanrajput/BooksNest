2394b60acdcfeb3bab7aca1cc123631aacbbc150
Aman Kumar <aman.it360@gmail.com>
2025-06-16 07:28:30 +0530

    Minor Changes

---
+++ backend/controllers/profile.controller.js
@@ -0,0 +1,38 @@
+const BorrowRecord = require("../models/borrowRecord.model");
+const User = require("../models/user.model");
+
+// get Borrow books Count Stats
+exports.getProfileStats = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    // Parallel promises
+    const [issuedBooksCount, returnedBooksCount, user, overdueBooksCount] =
+      await Promise.all([
+        BorrowRecord.countDocuments({ userId, status: "issued" }),
+        BorrowRecord.countDocuments({ userId, status: "returned" }),
+        User.findById(userId).select("+fineAmount"),
+        BorrowRecord.countDocuments({
+          userId,
+          dueDate: { $lt: new Date() },
+          status: "issued",
+        }),
+      ]);
+
+    return res.status(200).json({
+      success: true,
+      data: {
+        issuedBooksCount,
+        returnedBooksCount,
+        totalFines: user?.fineAmount || 0,
+        overdueBooksCount,
+      },
+    });
+  } catch (error) {
+    console.error("Error in getProfileStats:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};

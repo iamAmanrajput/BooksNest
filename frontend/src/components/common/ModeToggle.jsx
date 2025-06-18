598c3a0856517819d6726d9e2d09f1354c4bcf28
Aman Kumar <aman.it360@gmail.com>
2025-06-18 22:55:24 +0530

    ModeToggle Updated

--- frontend/src/components/common/ModeToggle.jsx
+++ frontend/src/components/common/ModeToggle.jsx
@@ -15,9 +15,11 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon" className={`cursor-pointer`}>
-          <Sun className="h-[2rem] w-[2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[2rem] w-[2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Button variant="outline" size="icon" className="relative h-9 w-9">
+          <div className="relative h-4 w-4">
+            <Sun className="absolute h-4 w-4 rotate-0 scale-100 transition-transform dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-4 w-4 rotate-90 scale-0 transition-transform dark:rotate-0 dark:scale-100" />
+          </div>
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>

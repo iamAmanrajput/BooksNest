d2cd3873e9c1316c32dbe379d2de37a7f8bf9b5e
Aman Kumar <aman.it360@gmail.com>
2025-06-18 07:27:41 +0530

    User Sidebar Updated

--- frontend/src/components/user/UserSidebar.jsx
+++ frontend/src/components/user/UserSidebar.jsx
@@ -1,13 +1,12 @@
 import {
   BookOpen,
-  ChartBar,
-  FilePlus2Icon,
-  GalleryVerticalEnd,
-  PackageSearch,
-  Settings,
+  CornerDownLeft,
+  Heart,
+  History,
+  Home,
+  User,
 } from "lucide-react";
 import { Link, useLocation } from "react-router-dom";
-import { Button } from "@/components/ui/button";
 
 import {
   Sidebar,
@@ -20,43 +19,26 @@ import {
   SidebarFooter,
 } from "@/components/ui/sidebar";
 
-// Menu items.
+// Menu items
 const items = [
-  {
-    title: "Create Products",
-    url: "/admin/dashboard",
-    icon: FilePlus2Icon,
-  },
-  {
-    title: "All Products",
-    url: "/admin/dashboard/all-products",
-    icon: GalleryVerticalEnd,
-  },
-  {
-    title: "Orders",
-    url: "/admin/dashboard/orders",
-    icon: PackageSearch,
-  },
-  {
-    title: "Analytics",
-    url: "/admin/dashboard/analytics",
-    icon: ChartBar,
-  },
-  {
-    title: "Settings",
-    url: "/admin/dashboard/settings",
-    icon: Settings,
-  },
+  { title: "Dashboard", url: "/", icon: Home },
+  { title: "Browse Books", url: "/books", icon: BookOpen },
+  { title: "My History", url: "/history", icon: History },
+  { title: "Return Books", url: "/return/book", icon: CornerDownLeft },
+  { title: "Whislist", url: "/whislist", icon: Heart },
+  { title: "My Profile", url: "/profile", icon: User },
 ];
 
 const UserSidebar = () => {
   const { pathname } = useLocation();
+
   return (
     <Sidebar>
-      <SidebarHeader className="flex flex-row items-center gap-3">
+      <SidebarHeader className="flex flex-row items-center gap-3 mt-4 mb-4 pl-4">
         <BookOpen className="text-customblue" />
         <h3 className="text-xl font-bold">BooksNest</h3>
       </SidebarHeader>
+
       <SidebarContent>
         <SidebarGroupContent>
           <SidebarMenu>
@@ -65,11 +47,14 @@ const UserSidebar = () => {
                 <SidebarMenuButton
                   asChild
                   className={`${
-                    pathname === item.url && "bg-zinc-200 dark:bg-zinc-600"
+                    pathname === item.url ? "bg-zinc-200 dark:bg-zinc-600" : ""
                   }`}
                 >
-                  <Link to={item.url}>
-                    <item.icon />
+                  <Link
+                    to={item.url}
+                    className="flex items-center gap-3 px-4 py-5"
+                  >
+                    <item.icon size={28} />
                     <span>{item.title}</span>
                   </Link>
                 </SidebarMenuButton>
@@ -78,8 +63,9 @@ const UserSidebar = () => {
           </SidebarMenu>
         </SidebarGroupContent>
       </SidebarContent>
-      <SidebarFooter>
-        <Button className="cursor-pointer">Logout</Button>
+
+      <SidebarFooter className="border-t border-b text-center p-4">
+        <h1 className="font-bold text-xl">Aman Kumar</h1>
       </SidebarFooter>
     </Sidebar>
   );
--- frontend/src/pages/user/HomePage.jsx
+++ frontend/src/pages/user/HomePage.jsx
@@ -2,7 +2,9 @@ import Navbar from "@/components/common/Navbar";
 import React from "react";
 
 const HomePage = () => {
-  return <div className="w-full"></div>;
+  return <div className="w-full">
+    
+  </div>;
 };
 
 export default HomePage;

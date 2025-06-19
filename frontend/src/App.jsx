587eca0627d9b6f4664169c5caae47f5fd5711ed
Aman Kumar <aman.it360@gmail.com>
2025-06-19 16:52:43 +0530

    Files Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import HomePage from "./pages/user/HomePage";
 import UserLayout from "./layouts/UserLayout";
+import { Route, Routes } from "react-router-dom";
+import Signup from "./pages/user/Signup";
+import Login from "./pages/user/Login";
+import AdminLogin from "./pages/admin/AdminLogin";
 
 const App = () => {
   return (
     <div className="w-screen min-h-screen">
-      <UserLayout>
-        <HomePage />
-      </UserLayout>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <UserLayout>
+              <HomePage />
+            </UserLayout>
+          }
+        />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+
+        {/* Admin Routes */}
+        <Route path="/admin/login" element={<AdminLogin />} />
+      </Routes>
     </div>
   );
 };

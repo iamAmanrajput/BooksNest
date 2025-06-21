2d529e862aa9d63d3773eb2c55ae01aaaf88ffe2
Aman Kumar <aman.it360@gmail.com>
2025-06-21 15:03:41 +0530

    New Route Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,15 @@ import UserLayout from "./layouts/UserLayout";
 import Signup from "./pages/user/Signup";
 import Login from "./pages/user/Login";
 import AdminLogin from "./pages/admin/AdminLogin";
+import Book from "./pages/user/Book";
 
 const App = () => {
   return (
     <div className="min-h-screen w-full bg-white text-zinc-900 dark:bg-zinc-900 dark:text-zinc-100 transition-colors duration-300">
       <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Login />} />
+
         <Route
           path="/"
           element={
@@ -18,8 +22,15 @@ const App = () => {
             </UserLayout>
           }
         />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" element={<Login />} />
+
+        <Route
+          path="/book/:id"
+          element={
+            <UserLayout>
+              <Book />
+            </UserLayout>
+          }
+        />
 
         {/* Admin Routes */}
         <Route path="/admin/signin" element={<AdminLogin />} />

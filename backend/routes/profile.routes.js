9eb42e128f4534c7d8070009d825cc1f5738ca56
Aman Kumar <aman.it360@gmail.com>
2025-06-30 17:34:18 +0530

    feat: add current user api route

--- backend/routes/profile.routes.js
+++ backend/routes/profile.routes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { getProfileStats } = require("../controllers/profile.controller");
+const {
+  getProfileStats,
+  getCurrentUserData,
+} = require("../controllers/profile.controller");
 const { isLoggedIn, isAdmin } = require("../middlewares/verifyToken");
 
 router.get("/stats", isLoggedIn, getProfileStats);
+router.get("/currentUser", isLoggedIn, getCurrentUserData);
 
 module.exports = router;

e925d6b51f1d8e2de90ea6e71845c8c04d151128
Aman Kumar <aman.it360@gmail.com>
2025-07-01 21:07:15 +0530

    feat: add profile update api route

--- backend/routes/profile.routes.js
+++ backend/routes/profile.routes.js
@@ -4,10 +4,16 @@ const router = express.Router();
 const {
   getProfileStats,
   getCurrentUserData,
+  updateProfile,
+  updatePassword,
+  updateProfilePic,
 } = require("../controllers/profile.controller");
 const { isLoggedIn, isAdmin } = require("../middlewares/verifyToken");
 
 router.get("/stats", isLoggedIn, getProfileStats);
 router.get("/currentUser", isLoggedIn, getCurrentUserData);
+router.patch("/updateProfile", isLoggedIn, updateProfile);
+router.patch("/updatePassword", isLoggedIn, updatePassword);
+router.patch("/updateProfilePic", isLoggedIn, updateProfilePic);
 
 module.exports = router;

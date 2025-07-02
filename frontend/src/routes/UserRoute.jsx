31a14fa597973de66e5bac8baeef1338533f65c8
Aman Kumar <aman.it360@gmail.com>
2025-07-02 21:17:19 +0530

    feat: add UserRoute component to protect user routes

---
+++ frontend/src/routes/UserRoute.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Navigate, useLocation } from "react-router-dom";
+import { setUserLogout } from "@/redux/slices/authSlice";
+
+const UserRoute = ({ children }) => {
+  const dispatch = useDispatch();
+  const { isAuthenticated, role } = useSelector((state) => state.auth);
+  const { pathname } = useLocation();
+  const [forceLogout, setForceLogout] = useState(false);
+
+  useEffect(() => {
+    if (isAuthenticated && role !== "user") {
+      dispatch(setUserLogout());
+      setForceLogout(true);
+    }
+  }, [isAuthenticated, role, dispatch]);
+
+  if (forceLogout) {
+    return <Navigate to="/signin" replace />;
+  }
+
+  if (!isAuthenticated) {
+    return (
+      <Navigate to="/signin" state={{ from: location.pathname }} replace />
+    );
+  }
+
+  if (["/signin", "/signup"].includes(pathname)) {
+    return <Navigate to="/" />;
+  }
+
+  return children;
+};
+
+export default UserRoute;

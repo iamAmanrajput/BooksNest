ffa47c9f518e75763c2cf7e238727342fd2cf7b2
Aman Kumar <aman.it360@gmail.com>
2025-07-03 15:13:35 +0530

    feat: add admin schema

---
+++ backend/models/admin.model.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const adminSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: [true, "Username is required"],
+      trim: true,
+      lowercase: true,
+      minlength: [3, "Username must be at least 3 characters long"],
+      maxlength: [10, "Username must be at most 10 characters long"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      select: false,
+      minlength: [6, "Password must be at least 6 characters long"],
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: {
+        values: ["admin"],
+        message: "Role must be admin",
+      },
+      default: "admin",
+    },
+  },
+  { timestamps: true }
+);
+
+// Hash password before saving
+adminSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Compare password method
+adminSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model("Admin", adminSchema);

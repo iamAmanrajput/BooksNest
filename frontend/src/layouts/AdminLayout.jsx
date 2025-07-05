66d2c4cde54a4fc45f39aef5fb70355778d30dd0
Aman Kumar <aman.it360@gmail.com>
2025-07-05 22:28:46 +0530

    feat: create admin layout

--- frontend/src/layouts/AdminLayout.jsx
+++ frontend/src/layouts/AdminLayout.jsx
@@ -1,7 +1,30 @@
+// UserLayout.jsx
 import React from "react";
+import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
+import Footer from "@/components/user/Footer";
+import AdminNavbar from "@/components/admin/common/AdminNavbar";
+import AdminSidebar from "@/components/admin/common/AdminSidebar";
 
-const AdminLayout = () => {
-  return <div>AdminLayout</div>;
+const AdminLayout = ({ children }) => {
+  return (
+    <SidebarProvider>
+      <div className="flex h-screen w-full overflow-hidden">
+        {/* Sidebar */}
+        <AdminSidebar />
+
+        {/* Content that adjusts automatically */}
+        <SidebarInset>
+          <main className="flex-1 overflow-y-auto overflow-x-hidden">
+            <div className="w-full max-w-full">
+              <AdminNavbar />
+              {children}
+              <Footer />
+            </div>
+          </main>
+        </SidebarInset>
+      </div>
+    </SidebarProvider>
+  );
 };
 
 export default AdminLayout;

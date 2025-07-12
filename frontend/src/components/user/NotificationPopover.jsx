4746924035849f211213483ab713b4577eb7aae6
Aman Kumar <aman.it360@gmail.com>
2025-07-12 21:58:50 +0530

    feat: animation added in badge

--- frontend/src/components/user/NotificationPopover.jsx
+++ frontend/src/components/user/NotificationPopover.jsx
@@ -164,7 +164,7 @@ const NotificationPopOver = () => {
           {unreadNotificationCount > 0 && (
             <Badge
               variant="destructive"
-              className="absolute -top-2 -right-2 h-5 w-5 flex items-center justify-center p-0 text-xs"
+              className="absolute -top-2 -right-2 h-5 w-5 animate-pulse flex items-center justify-center p-0 text-xs"
             >
               {unreadNotificationCount > 9 ? "9+" : unreadNotificationCount}
             </Badge>

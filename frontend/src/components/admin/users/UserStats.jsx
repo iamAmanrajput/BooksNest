e408c03df3379f5b3cc0d75bc37d21194b097a5b
Aman Kumar <aman.it360@gmail.com>
2025-07-22 07:43:46 +0530

    feat: implement users stats feature

--- frontend/src/components/admin/users/UserStats.jsx
+++ frontend/src/components/admin/users/UserStats.jsx
@@ -1,27 +1,57 @@
 import StatCard from "@/components/common/StatCard";
+import axios from "axios";
 import { UserCheck, Users, UserX } from "lucide-react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { toast } from "sonner";
 
 const UserStats = () => {
   const [loading, setLoading] = useState(false);
+  const [usersStats, setUsersStats] = useState(null);
+
+  useEffect(() => {
+    const fetchUsersStats = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_BACKEND_URL}/user/users/stats`,
+          {
+            withCredentials: true,
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+            },
+          }
+        );
+
+        if (response?.data?.success) {
+          setUsersStats(response.data.data);
+        }
+      } catch (error) {
+        console.error("Error fetching profile stats:", error);
+        toast.error(error?.response?.data?.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchUsersStats();
+  }, []);
   return (
     <div className="w-full grid grid-cols-1 sm:grid-cols-3 gap-3 mt-6">
       <StatCard
         icon={Users}
         title="Total Users"
-        value={loading ? null : 40}
+        value={loading ? null : usersStats?.totalUsers}
         color="blue"
       />
       <StatCard
         icon={UserCheck}
         title="Active Users"
-        value={loading ? null : 30}
+        value={loading ? null : usersStats?.unblockedUsers}
         color="green"
       />
       <StatCard
         icon={UserX}
         title="Blocked Users"
-        value={loading ? null : 10}
+        value={loading ? null : usersStats?.blockedUsers}
         color="red"
       />
     </div>

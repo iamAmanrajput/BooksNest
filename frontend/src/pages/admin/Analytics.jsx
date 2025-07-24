09d65fc2e16012857740f5b2d51c229990c60ff7
Aman Kumar <aman.it360@gmail.com>
2025-07-24 15:56:40 +0530

    feat: add BarChartsWrapper component to Analytics page

--- frontend/src/pages/admin/Analytics.jsx
+++ frontend/src/pages/admin/Analytics.jsx
@@ -1,4 +1,5 @@
 import AnalyticsStats from "@/components/admin/Analytics/AnalyticsStats";
+import BarChartsWrapper from "@/components/admin/Analytics/BarChartsWrapper";
 import DashboardInsights from "@/components/admin/Analytics/DashboardInsights";
 import DataPeriod from "@/components/admin/Analytics/DataPeriod";
 import React from "react";
@@ -8,6 +9,7 @@ const Analytics = () => {
     <div className="px-4 pb-6 flex flex-col gap-6 bg-zinc-100 dark:bg-[#09090b] text-zinc-900 dark:text-zinc-100">
       <AnalyticsStats />
       <DataPeriod />
+      <BarChartsWrapper />
       <DashboardInsights />
     </div>
   );
